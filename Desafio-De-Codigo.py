6762e463bada55132008fa4e44b1f0ba74fea33d
silclementino <167935100+silclementino@users.noreply.github.com>
2024-05-26 19:53:34 -0300

    Create Desafio-De-Codigo.py

---
+++ Desafio-De-Codigo.py
@@ -0,0 +1,28 @@
+# Dicionário associando características aos modelos Claude 3
+caracteristicas_modelos = {
+    "automatizar tarefas": "Claude 3 Opus",
+    "pesquisa e desenvolvimento": "Claude 3 Opus",
+    "resposta rápida e capacidade de resposta quase instantânea": "Claude 3 Haiku",
+    "motores de chatbots de lojas": "Claude 3 Haiku",
+    "moderação de conteúdo": "Claude 3 Haiku",
+    "processamento de tarefas mais básicas": "Claude 3 Haiku",
+    "raciocínio cuidadoso": "Claude 3 Sonnet",
+    "processamento de dados": "Claude 3 Sonnet",
+    "aplicações de vendas": "Claude 3 Sonnet",
+    "extração de texto de imagens": "Claude 3 Sonnet",
+    "equilíbrio ideal entre inteligência e velocidade": "Claude 3 Sonnet",
+   
+}
+
+def encontrar_modelo(caracteristica):
+    for chave, valor in caracteristicas_modelos.items():
+        if chave.lower() in caracteristica.lower():
+            return valor
+    return "modelo não foi encontrado"
+
+# Entrada do usuário
+caracteristica_fornecida = input()
+
+# Encontrar e imprimir o modelo correspondente
+modelo_correspondente = encontrar_modelo(caracteristica_fornecida)
+print(modelo_correspondente)
